d26964955719c61871ea294cac65d399aa35d6bd
anuarthr <anuarthrincongonzalez@gmail.com>
2025-12-01 16:29:07 -0500

    feat: add user authentication with JWT and custom user model

---
+++ users/models.py
@@ -0,0 +1,29 @@
+from django.db import models
+from django.contrib.auth.models import AbstractBaseUser, PermissionsMixin
+from .managers import CustomUserManager
+
+class User(AbstractBaseUser, PermissionsMixin):
+    class Role(models.TextChoices):
+        ADMIN = "ADMIN", "Administrador"
+        CLIENT = "CLIENT", "Cliente"
+        WORKER = "WORKER", "Trabajador"
+        COMPANY = "COMPANY", "Empresa"
+
+    email = models.EmailField(unique=True)
+    first_name = models.CharField(max_length=150, blank=True)
+    last_name = models.CharField(max_length=150, blank=True)
+    role = models.CharField(max_length=50, choices=Role.choices, default=Role.CLIENT)
+    
+    avatar = models.ImageField(upload_to='avatars/', null=True, blank=True)
+
+    is_staff = models.BooleanField(default=False)
+    is_active = models.BooleanField(default=True)
+    date_joined = models.DateTimeField(auto_now_add=True)
+
+    USERNAME_FIELD = 'email'
+    REQUIRED_FIELDS = []
+
+    objects = CustomUserManager()
+
+    def __str__(self):
+        return f"{self.email} ({self.role})"
