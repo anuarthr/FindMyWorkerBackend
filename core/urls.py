ad788334f454b9981e67ebd6ee9028d0b5c042aa
anuarthr <anuarthrincongonzalez@gmail.com>
2025-12-01 17:13:42 -0500

    feat: add CORS middleware and configuration to settings

--- core/urls.py
+++ core/urls.py
@@ -7,7 +7,7 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     
     # Auth: Login, Refresh y Registro
-    path('api/auth/register/', RegisterView.as_view(), name='register'), # <--- NUEVA RUTA
+    path('api/auth/register/', RegisterView.as_view(), name='register'),
     path('api/auth/login/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
     path('api/auth/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
     path('api/users/', include('users.urls')),
